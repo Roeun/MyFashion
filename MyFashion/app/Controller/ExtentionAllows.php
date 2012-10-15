a2537460b61e53827bd2bbc610c6b8457d6ea291
Roeun <chhorn.riseup01@jcitsp.asia>
2012-10-15 10:18:27 +0900

    First Upload

---
+++ MyFashion/app/Controller/ExtentionAllows.php
@@ -0,0 +1,12 @@
+<?php
+
+class ExtentionAllowsController extends AppController { 
+    
+    function isAllowExtention($extention_type) {
+        return false;
+    }
+    
+}
+
+
+?>
---
+++ MyFashion/app/Controller/SecuritiesController.php
@@ -0,0 +1,40 @@
+<?php
+
+class SecuritiesController extends AppController { 
+  
+    function encrypt_userdata($data) {
+        return $data;
+    }
+    
+    function decrypt_userdata($data) {
+        return $data;
+    }
+    
+    function encrypt_session($data) {
+        return $data;
+    }
+    
+    function decrypt_session($data) {
+        return $data;
+    }
+    
+    function block_ip($ip, $session_id) {
+        return false;
+    }
+    
+    function user_limitation($email, $ip) {
+        return false;
+    }
+    
+    function sql_injection_input($data) {
+        return $data;
+    }
+    
+    function sql_injection_output($data) {
+        return $data;
+    }
+    
+    
+}
+
+?>
