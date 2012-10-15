4f0a6549e7972603956e93fb220dee6346481675
Roeun <chhorn.riseup01@jcitsp.asia>
2012-10-15 18:23:07 +0900

    Security Model

--- MyFashion/app/Model/Security.php
+++ MyFashion/app/Model/Security.php
@@ -3,23 +3,54 @@ class Security extends AppModel {
     
     public $name = 'Security';    
       
-    function encrypt_userdata($data) {
+    public $key = "scret key";
+    
+    function encrypt_userdata($string) {
+        $result = '';
+        for($i=0; $i<strlen($string); $i++) {
+          $char = substr($string, $i, 1);
+          $keychar = substr($key, ($i % strlen($key))-1, 1);
+          $char = chr(ord($char)+ord($keychar));
+          $result.=$char;
+        }
+        return base64_encode($result);
+    }
+    
+    function decrypt_userdata($string) {
+        $result = '';
+        $string = base64_decode($string);
+        for($i=0; $i<strlen($string); $i++) {
+          $char = substr($string, $i, 1);
+          $keychar = substr($key, ($i % strlen($key))-1, 1);
+          $char = chr(ord($char)-ord($keychar));
+          $result.=$char;
+        }
+        return $result;
+    }
+    
+    function encrypt_session($string) {
+        $data = base64_encode($string);
+        $data = str_replace(array('+','/','='),array('-','_',''),$data);
         return $data;
     }
     
-    function decrypt_userdata($data) {
-        return $data;
-    }
-    
-    function encrypt_session($data) {
-        return $data;
-    }
-    
-    function decrypt_session($data) {
-        return $data;
+    function decrypt_session($string) {
+        $data = str_replace(array('-','_'),array('+','/'),$string);
+        $mod4 = strlen($data) % 4;
+        if ($mod4) {
+            $data .= substr('====', $mod4);
+        }
+        return base64_decode($data);
     }
     
     function block_ip($ip, $session_id) {
+        static $try;
+        if ($_SESSION['ip']==$ip) {
+            $try++;
+            if ($try >= 5) {
+                return true;
+            }
+        }
         return false;
     }
     
