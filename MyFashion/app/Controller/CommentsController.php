b3de90367b6ed415aa38de50c7c946675433054f
chanratha-ly <chanratha.ly@gmail.com>
2012-10-15 11:36:08 +0900

    Add

---
+++ MyFashion/app/Controller/CommentsController.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of CommentsController
+ *
+ * @author apple
+ */
+class CommentsController extends AppController {
+    public $helpers = array('Form','Html');
+    
+    public function index () {
+        
+    }
+
+    public  function insert_comment () {
+        
+    }
+    
+    public function delete_comment () {
+        
+    }
+    
+    public function count_comment () {
+        
+    }
+}
+
+?>
