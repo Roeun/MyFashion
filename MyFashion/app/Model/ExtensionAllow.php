5b8c1730122a7ad3950f14cd0a19311c90ae78cd
Roeun <chhorn.riseup01@jcitsp.asia>
2012-10-17 10:28:54 +0900

    Update MyFashion/app/Model/ExtensionAllow.php
    Update Extension Allow

--- MyFashion/app/Model/ExtensionAllow.php
+++ MyFashion/app/Model/ExtensionAllow.php
@@ -2,8 +2,15 @@
 
 class ExtensionAllow extends AppModel {
     
+    public $useTable = "extensionallows";
+    public $name = "ExtensionAllow";
+    
     function isAllowExtention($extention_type) {
-        return false;
+        $extention_type = strtolower($extention_type);
+        $conditions = array(
+            'exname'=>$extention_type
+        );
+        return $this->find('all', array('conditions'=>$conditions));
     }
     
 }
