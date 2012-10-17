c078d8242018e4bb85d54432a6b4e755480fad01
bunchhean <moonbunchheang@gmail.com>
2012-10-17 15:16:53 +0900

    Update MyFashion/app/Controller/UsersController.php
    User Class Upload profile images

--- MyFashion/app/Controller/UsersController.php
+++ MyFashion/app/Controller/UsersController.php
@@ -7,10 +7,12 @@
  */
 
 App::uses('AppController', 'Controller');
-
+include 'resize_class.php';
 class UsersController extends AppController {
         public $helpers = array('Html','Form');
+        public $uses = array('User', 'Security');
 
+        
         /////////////////// function used to create an account for use website ///////
         public function register(){
             $title_for_layout = 'Create an Account';
@@ -19,10 +21,13 @@ class UsersController extends AppController {
             // if the form was submitted
             if(!empty($this->data)) {     
                 $my_date = date('Y-m-d');
+                $myUname = $this->request->data('User.uname');
+                $myPass = $this->Security->encrypt_userdata($this->request->data('User.pwd'));
+                $myEmail = $this->request->data('User.email');
                 $arr = array (
-                    'uname' => $this->request->data('User.uname'),
-                    'pwd' => $this->request->data('User.pwd'),
-                    'email' => $this->request->data('User.email'),
+                    'uname' => $myUname,
+                    'pwd' => $myPass,
+                    'email' => $myEmail,
                     'gender' => $this->request->data('User.gender'),
                     'phone' => $this->request->data('User.phone'),
                     'dob' => $this->request->data('User.dob'),
@@ -60,11 +65,13 @@ class UsersController extends AppController {
                     $this->set(compact('title_for_layout')); 
                     // if the form was submitted
                     if(!empty($this->data)) {
+                                $myPass = $this->Security->encrypt_userdata($this->data['User']['pwd']);
+                                $myEmail = $this->data['User']['email'];
                                // find the user in the database
-                               $dbuser = $this->User->findByEmail($this->data['User']['email']);
+                               $dbuser = $this->User->findByEmail($myEmail);
                                $userIp = $this->request->clientIp();
                                // if found and passwords match
-                               if(!empty($dbuser) && ($dbuser['User']['pwd'] == $this->data['User']['pwd']) && ($dbuser['User']['isenable'] == '1')) {
+                               if(!empty($dbuser) && ($dbuser['User']['pwd'] == $myPass) && ($dbuser['User']['isenable'] == '1')) {
                                        // write the username to a session
                                        $this->Session->write('User', $dbuser);
                                      //  $this->Session->write('User', $userIp);
@@ -92,22 +99,86 @@ class UsersController extends AppController {
         ///////////// function reset password when user forgot password ///////////
         public function reset_password(){
              AppController::myCheckSession();
+             if(!empty($this->data)){
+                $this->Session->setFlash('Please check your email. Thank you!');
+             }
         }
         
         //////////////////  function update profile information ///////////////////
-        public function update_profile(){
-             AppController::myCheckSession();
-            
+        public function update_profile($id = null){
+                AppController::myCheckSession();
+                $title_for_layout = 'Update Profile';
+                $this->set(compact('title_for_layout'));  
+
+
+                $this->User->id  = $id;
+
+                if(empty($this->data)){
+                    $this->data= $this->User->read();
+                }else{     
+                    $myUname = $this->request->data('User.uname');
+                    $myPass = $this->Security->encrypt_userdata($this->request->data('User.pwd'));
+                    $myEmail = $this->request->data('User.email');
+                    $arr = array (
+                        'uname' => $myUname,
+                        'pwd' => $myPass,
+                        'email' => $myEmail,
+                        'gender' => $this->request->data('User.gender'),
+                        'phone' => $this->request->data('User.phone'),
+                        'dob' => $this->request->data('User.dob')
+                    ); 
+
+                    if($this->User->save($arr)){
+                        $this->Session->setFlash('Your profile have been update!');
+                        $this->redirect(array('action' => 'my_profile'));
+                    }
+                }
         }
         
         ///////////  Function change password of user /////////
-        public function change_password(){
-             AppController::myCheckSession();
+        public function change_password($id = null){
+            AppController::myCheckSession();
+            $title_for_layout = 'Change Password';
+            $this->set(compact('title_for_layout'));  
+            
+            
+            $this->User->id  = $id;
+            
+            if(empty($this->data)){
+                $this->data= $this->User->read();
+            }else{  
+                $myPass = $this->Security->encrypt_userdata($this->request->data('User.pwd'));
+                $arr = array (
+                    'pwd' => $myPass
+                ); 
+
+                if($this->User->save($arr)){
+                    $this->Session->setFlash('Password have been change!');
+                    $this->redirect(array('action' => 'my_profile'));
+                }
+            }
         }
         
         ///////////// function upload your profile picture ///////////////////////
-        public function upload_photo_profile(){
-             AppController::myCheckSession();
+        public function upload_photo_profile($id = null){
+            AppController::myCheckSession();
+            if (!empty($this->data)) {
+                $photo["pname"] = $this->data["User"]["picture"]["name"];
+                $photo["pname_tmp"] = $this->data["User"]["picture"]["tmp_name"];
+                if (!$this->User->upload_photo_function($photo)) {
+                    $this->Session->setFlash ("Unable to save photo.");
+                } else {
+                    $this->User->id  = $id;
+                    $arr = array (
+                        'picture' => $this->data["User"]["picture"]["name"]
+                    ); 
+                    if($this->User->save($arr)){
+                        $this->Session->setFlash ("Picture saved!");
+                        $this->redirect(array('action' => 'my_profile'));
+                    }
+                    
+                } 
+            }
         }
         
         ////////////////  function disactivate account /////////////
